951031e3b11a36ead47c6f900b1b40453b064424
Tajuddin Tappe <tajuddin87@gmail.com>
2022-02-23 18:56:47 +0800

    Add project files.

---
+++ Assessment/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment
+{
+    class Account
+    {
+        int ID;
+        string NAME;
+        double BALANCE;
+
+        public Account()
+        {
+
+        }
+
+        public Account(int id, string name, double balance)
+        {
+
+        }
+    }
+}
